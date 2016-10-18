b25afb67bb84300bbd90e02746ce9d74804a3613
Andres Ceballos Sanchez <Andres Ceballos Sanchez>
2016-10-18 12:33:02 -0500

    Canvas creado

---
+++ Universidad-1.0/Assets/Scripts/GameTime.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameTime : MonoBehaviour {
+
+	[HideInInspector]
+	public Text textTime; //texto que aparece en la pantalla.
+	public int time;  //tiempo en segundos
+
+	//Inicialización de variables
+	void Start () {
+		textTime = GetComponent<Text> ();
+		time = 180;
+		textTime.text = formatTime (time);
+		startTime ();
+	}
+
+	void startTime(){
+		InvokeRepeating ("decreaseTime", 1, 1);
+	}
+
+	/*Reducir el tiempo cada segundo*/
+	void decreaseTime(){		
+		textTime.text = formatTime (--time);
+	}
+
+	/*Toma el tiempo actual en segundos y lo formatea para ser mostrado en la pantalla como min:sec*/
+	string formatTime(int currenTime){
+		int minutes, seconds;
+
+		/*Conversion a minutos y segundos*/
+		minutes = currenTime / 60;
+		seconds = currenTime % 60;
+
+		/*Crear un string con el tiempo a mostrar*/
+		string formattedText = string.Format("{0}:{1}",minutes.ToString ("D2"),seconds.ToString ("D2"));
+		return formattedText;
+	}
+
+	/*Pausar el tiempo*/
+	public void pauseTime(){
+		CancelInvoke ();
+	}
+
+	/*Reanudar el tiempo*/
+	public void restartTime(){
+		InvokeRepeating ("decreaseTime", 0, 1);
+	}
+}
---
+++ Universidad-1.0/Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+
+	Slider mySlider;
+	bool increasing;
+
+	void Start(){
+		mySlider = GetComponent<Slider> ();
+		increasing = true;
+	}
+
+	public void Update(){
+		if (increasing) {
+			mySlider.value += 0.01f;
+		} else {
+			mySlider.value -= 0.01f;
+		}
+
+		if (mySlider.value == mySlider.maxValue || mySlider.value == mySlider.minValue) {
+			increasing = !increasing;
+		}
+	}
+
+
+
+}
